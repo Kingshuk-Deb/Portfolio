411ab1058d8434faa2c771f4241885fa456292c1
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2021-09-13 12:53:38 +0530

    Create constants.js

---
+++ src/constants/constants.js
@@ -0,0 +1,40 @@
+export const projects = [
+  {
+    title: 'Crypto Tracker',
+    description: "You can search for all the different cryptos and find about their current status. As you click on the cryto you are automatically redirected to coingecko for further information on the crypto",
+      image: '/images/1.png',
+    source: 'https://github.com/Kingshuk-Deb/Crypto-Tracker',
+    visit: 'https://github.com/Kingshuk-Deb/Crypto-Tracker',
+    id: 0,
+  },
+  {
+    title: 'Netflix Clone',
+    description:"This project is a simplified front end clone of Netflix. It was created with React.JS. It uses The MovieDB Api(TMDB) to search for movies and display details. Users can the see upcoming and trending movies. Data updated on a weekly basis. Users can click on a movie and the trailer of the series/movie would be shown. The webpage adapts to any screen size.",
+    image: '/images/2.png',
+    source: 'https://github.com/Kingshuk-Deb/Netflix-Clone',
+    visit: 'https://github.com/Kingshuk-Deb/Netflix-Clone',
+    id: 1,
+  },
+  {
+    title: 'ToDo App',
+    description: "A Modern Todo App made with React, also uses Local Storage of the browser.",
+      image: '/images/3.png',
+    source: 'https://github.com/Kingshuk-Deb/TodoApp-React',
+    visit: 'https://github.com/Kingshuk-Deb/TodoApp-React',
+    id: 2,
+  },
+  {
+    title: 'Portfolio Website',
+    description: "Modern Portfolio Website made using Next JS and React Hooks.",
+      image: '/images/4.png',
+    source: 'https://github.com/Kingshuk-Deb/Portfolio',
+    visit: 'https://github.com/Kingshuk-Deb/Portfolio',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: 'Started my journey', },
+  { year: 2021, text: 'Working on my Web Development/Design Skills', },
+  { year: 2022, text: 'Planning to launch a student hub bridging the current gap', },
+];
