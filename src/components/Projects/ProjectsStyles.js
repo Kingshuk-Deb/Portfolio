de64c4209ed271f6f010db7b0c5dc45e3e8346be
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2021-09-13 12:51:04 +0530

    Add files via upload

---
+++ src/components/Projects/ProjectsStyles.js
@@ -0,0 +1,115 @@
+import styled from 'styled-components';
+
+export const Img = styled.img`
+  width:100%;
+  height:100%;
+  object-fit: cover;
+  overflow: hidden;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`
+
+export const GridContainer = styled.section`
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+padding: 3rem;
+place-items: center;
+column-gap: 2rem;
+row-gap: 3rem;
+@media ${(props) => props.theme.breakpoints.sm} {
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  padding-bottom: 0;
+}
+
+`
+export const BlogCard = styled.div`
+  border-radius: 20px;
+  background-color: #212D45;
+  box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
+  text-align: center;
+  width: 400px;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    width: 100%;
+  }
+`;
+export const TitleContent = styled.div`
+  text-align: center;
+  z-index: 20;
+  width: 100%;
+
+`;
+
+
+export const HeaderThree = styled.h3`
+  font-weight: 500;
+  letter-spacing: 2px;
+  color: #9cc9e3;
+  padding: .5rem 0;
+  font-size: ${(props) => props.title ? '3rem' : '2rem'};
+`;
+
+export const Hr = styled.hr`
+  width: 100px;
+  height: 3px;
+  margin: 10px auto;
+  margin-bottom: 20px;
+  border: 0;
+  background: #fff;
+`;
+
+export const Intro = styled.div`
+  width: 170px;
+  margin: 0 auto;
+  color: #dce3e7;
+  font-family: 'Droid Serif', serif;
+  font-size: 13px;
+  font-style: italic;
+  line-height: 18px;
+`;
+
+
+export const CardInfo = styled.p`
+  width: 100%;
+  padding: 0 50px;
+  color: #e4e6e7;
+  font-style: 2rem;
+  line-height: 24px;
+  text-align: justify;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    padding: 1.5rem
+  }
+`;
+
+
+export const UtilityList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: space-around;
+  margin: 2.5rem 0;
+`;
+
+export const ExternalLinks = styled.a`
+color:#000;
+letter-spacing: 2px;
+font-size: 1.6rem;
+padding: 1rem 2.5rem;
+background: linear-gradient(270deg,#13ADC7 0%,#945DD6 100%);
+border-radius: 5px;
+transition: .4s ease;
+&:hover{
+  background: linear-gradient(270deg,#00DBD8 0%,#B133FF 100%);
+}
+`;
+
+export const TagList = styled.ul`
+display: flex;
+justify-content: space-around;
+padding: 2rem;
+`
+export const Tag = styled.li`
+color: #d8bfbf;
+font-size: 1.5rem;
+`;
